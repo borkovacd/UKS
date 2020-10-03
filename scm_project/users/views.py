dc943cd348da5b7b9948eb153676377bb7e807e4
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2020-10-03 20:43:24 +0200

    Simple Profile page

--- scm_project/users/views.py
+++ scm_project/users/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here :
+
+def home(request):
+    return render(request, 'users/home.html')
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -15,6 +20,6 @@ def register(request):
         form  = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
-def home(request):
-    return render(request, 'users/home.html')
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
