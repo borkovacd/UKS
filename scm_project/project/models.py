d2eef668abed20b9020eae45b46684582f9cb169
olgaSavic <lola.savi4@gmail.com>
2020-10-24 21:24:45 +0200

    Small changes in model

--- scm_project/project/models.py
+++ scm_project/project/models.py
@@ -65,7 +65,7 @@ class Milestone(models.Model):
 
 class Collaborator(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null = True)
 
     def get_absolute_url(self):
         return reverse('collaborator-detail', kwargs={'pk': self.pk})
