9105d874590b83da0617181a73e93679ea9d81fb
olgaSavic <lola.savi4@gmail.com>
2020-11-01 04:51:03 +0100

    Added django-heroku

--- scm_project/scm_project/settings.py
+++ scm_project/scm_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,5 +134,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
 
 
